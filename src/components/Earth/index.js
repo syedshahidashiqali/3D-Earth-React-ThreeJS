99b50be0c1f630123282bb811810eee0ff7b8ecd
Syed Shahid Ali <syedshahid1215@gmail.com>
2022-06-29 17:47:04 +0500

    add earh textures (colorMap, normalMap, specularMap)

--- src/components/Earth/index.js
+++ src/components/Earth/index.js
@@ -1,10 +1,34 @@
+// importing textures
+import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
+import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg';
+import EarthSpecularMap from '../../assets/textures/8k_earth_specular_map.jpg';
+import EarthCloudsMap from '../../assets/textures/8k_earth_clouds.jpg';
+
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+import { OrbitControls } from '@react-three/drei';
+
 function Earth(props) {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
   return (
     <>
       <ambientLight intensity={1} />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
-        <meshPhongMaterial color='red' />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
       </mesh>
     </>
   );
